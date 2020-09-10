d0bbf8b752846d463fba69d2a1208d9782820585
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-10 14:25:46 -0700

    Step 2 Part 1 complete and cleared up a couple bugs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 
-modeul.exports = {
+module.exports = {
 
     register: async (req, res) => {
         const {username, password, isAdmin} = req.body
@@ -9,14 +9,24 @@ modeul.exports = {
         const result = await db.get_user([username])
         const existingUser = result[0]
 
-        if(existingUser[0]) {
+        if(existingUser) {
             return res.status(409).send('Username taken.')
         }
 
         const salt = bcrypt.genSaltSync(10)
         const hash = bcrypt.hashSync(password, salt)
-        
 
+        const registeredUser = await db.register_user([isAdmin, username, hash])
+
+        const user = registeredUser[0]
+        req.session.user = {
+            isAdmin: user.is_admin,
+            id: user.id,
+            username: user.username
+        }
+
+        res.status(201).send(req.session.user)
     }
+
 
 }
