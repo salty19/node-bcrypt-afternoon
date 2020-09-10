5e6fd31eaab75ad19593ad0a455379564bfa663c
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-10 15:08:38 -0700

    reigster button and alert works

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -34,6 +34,22 @@ export default class Header extends Component {
 
   register() {
     // axios POST to /auth/register here
+    const { username, password, isAdmin } = this.state
+    axios.post('/auth/register', {username, password, isAdmin})
+    .then((res) => {
+      this.setState({
+        username: '',
+        password: ''
+      })
+      this.props.updateUser(res.data)
+    })
+    .catch(err => {
+      this.setState({ 
+        username: '',
+        password: ''
+      })
+      alert(err.response.request.response)
+    })
   }
 
   logout() {
