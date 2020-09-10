eb76d8593fbf6e6de3983b0fd9f6bdfffaf76f3a
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-10 13:00:58 -0700

    Step 1 Parts 1 and 2 complete

--- server/index.js
+++ server/index.js
@@ -3,9 +3,28 @@ const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
 
+
 const app = express()
 const PORT = 4000
 
-app.use(express.json())
+const { CONNECTION_STRING, SESSION_SECRET } = process.env
 
-app.listen(PORT, () => console.log(`LISTENING ON PORT ${PORT}!!!`))
+app.use(express.json())
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET,
+    })
+)
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then((dbInstance) => {
+    app.set('db', dbInstance)
+    console.log('This is the DB and it is set up')
+    app.listen(PORT, () => console.log(`LISTENING ON PORT ${PORT}!!!`))
+})
