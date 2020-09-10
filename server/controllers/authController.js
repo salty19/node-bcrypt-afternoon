0668f8ddaa1206d4545dc52a2b68ba5092009874
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-10 14:03:11 -0700

    stuck on register_user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,22 @@
 const bcrypt = require('bcryptjs')
+
+modeul.exports = {
+
+    register: async (req, res) => {
+        const {username, password, isAdmin} = req.body
+        const db = req.app.get('db')
+
+        const result = await db.get_user([username])
+        const existingUser = result[0]
+
+        if(existingUser[0]) {
+            return res.status(409).send('Username taken.')
+        }
+
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+        
+
+    }
+
+}
