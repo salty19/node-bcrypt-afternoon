fefc918052e5e68de708fbe2d7bbb99aa69ee31e
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-10 18:59:10 -0700

    Step 3 Part 1 complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -30,9 +30,25 @@ module.exports = {
 
     login: async (req, res) => {
         const {username, password} = req.body
-        const db = req.app.get('db')
+        const foundUser = await req.app.get('db').get_user([username])
+        const user = foundUser[0]
 
-        const foundUser = await db.get_user([username])
-        
+        if(!user) {
+            res.status(401).send('User not found. Please register as a new user before logging in.')
+        } 
+
+        const isAuthenticated = bcrypt.compareSync(password, user.hash)
+
+        if(!isAuthenticated) {
+            res.status(403).send('Incorrect password')
+        }
+
+        req.session.user = {
+            isAdmin: user.is_admin,
+            id: user.id,
+            username: user.username
+        }
+
+        res.status(200).send(req.session.user)
     }
 }
