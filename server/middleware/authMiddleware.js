034f4e3f50fb0e68ff8bccdba2fa576c09012601
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-14 16:02:53 -0700

    found more errors and is now fixed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -15,7 +15,7 @@ module.exports = {
     adminsOnly: (req, res, next) => {
         
         if(!req.session.user.isAdmin) {
-            res.status(403).send('You are not an admin - HAHA')
+            return res.status(403).send('You are not an admin - HAHA')
         }
         next()
     }
